be004d94b877d6946c9a7f9b064a6d38e6ef9c51
MrSparkzz <xmrsparkzz@gmail.com>
2015-04-30 12:57:19 -0400

    Created a config manager class

--- src/net/sparkzz/servercontrol/ServerControl.java
+++ src/net/sparkzz/servercontrol/ServerControl.java
@@ -1,12 +1,16 @@
 package net.sparkzz.servercontrol;
 
 import com.google.inject.Inject;
+import net.sparkzz.servercontrol.config.Config;
 import net.sparkzz.servercontrol.util.Register;
 import net.sparkzz.servercontrol.util.Utility;
+import ninja.leaping.configurate.ConfigurationNode;
 import org.slf4j.Logger;
 import org.spongepowered.api.Server;
 import org.spongepowered.api.event.Subscribe;
 import org.spongepowered.api.event.state.PreInitializationEvent;
+import org.spongepowered.api.event.state.ServerStartingEvent;
+import org.spongepowered.api.event.state.ServerStoppingEvent;
 import org.spongepowered.api.plugin.Plugin;
 
 /**
@@ -16,22 +20,31 @@ import org.spongepowered.api.plugin.Plugin;
 @Plugin(id = "ServerControl", name = "Server Control", version = "0.1-DEV")
 public class ServerControl {
 
+	private Config config;
 	@Inject
-	private Logger logger;
+	private static Logger logger;
 	private static Server server;
 
-	public Logger getLogger() {
+	public static Logger getLogger() {
 		return logger;
 	}
 
 	@Subscribe
-	public void onServerStart(PreInitializationEvent event) {
+	public void onPreInit(PreInitializationEvent event) {
 		this.server = event.getGame().getServer();
+		this.config = new Config();
 
 		Utility.init(event.getGame(), logger);
 
 		new Register(this, server, event);
 
 		logger.info("Enabled successfully!");
+	}
+
+	@Subscribe
+	public void onServerStopping(ServerStoppingEvent event) {
+		config.unload();
+
+		logger.info("Disabled successfully!");
 	}
 }
